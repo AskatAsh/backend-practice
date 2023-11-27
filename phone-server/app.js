42c240f9e586485a3dcadba8c72e501789630bf4
AskatAsh <abdulaskat@gmail.com>
2023-11-27 20:30:48 +0600

    installed and run express server using nodemon

--- phone-server/app.js
+++ phone-server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('This is phone info server.');
+    res.send('This is phone info server using nodemon.');
 })
 
 app.listen(port, () => {
