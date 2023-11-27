c37cf277d71c17bd389e6c4fee790daf85dc0196
AskatAsh <abdulaskat@gmail.com>
2023-11-27 20:57:57 +0600

    used app.get method and require method to get phone API data

--- phone-server/app.js
+++ phone-server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('This is phone info server using nodemon.');
+})
+
+app.get('/phones', (req, res) => {
+    // res.send("get phone api data from phones.json.");
+    res.send(phones);
 })
 
 app.listen(port, () => {
