5b17f1ed7a7fac5ad15ebe59f39f0d666ffd3409
AskatAsh <abdulaskat@gmail.com>
2023-11-29 13:11:53 +0600

    using express nodemon and showing fake user data with server

---
+++ user-management-server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const users = [
+    {id: 1, name: "Arman", email: "arman@gmail.com"},
+    {id: 1, name: "Sakib", email: "sakib@gmail.com"},
+    {id: 1, name: "Jamil", email: "jamil@gmail.com"},
+]
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.get('/', (req, res) => {
+    res.send("User Management Server is running.");
+})
+
+app.listen(port, () => {
+    console.log(`Running user management server using port: ${port}`);
+})
