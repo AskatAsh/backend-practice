ef57dfa501c71d947a39cb3bc2539ab2d6a08297
AskatAsh <abdulaskat@gmail.com>
2023-11-29 23:17:59 +0600

    added button with click handler to get userid

--- mongo-crud-client/src/Components/Users.jsx
+++ mongo-crud-client/src/Components/Users.jsx
@@ -3,12 +3,20 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
     const users = useLoaderData();
 
+    const handleDelete = (_id) => {
+        console.log(_id);
+    }
     return (
         <div>
             <h2>Total Users: {users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>Name: {user.name}, Email: {user.email}</p>)
+                    users.map(user => <p key={user._id}>
+                        Name: {user.name}, 
+                        Email: {user.email} <button
+                        onClick={() => handleDelete(user._id)}
+                        >X</button>
+                        </p>)
                 }
             </div>
         </div>
