4dc3954587dbf9723e7a2164c004c92419149a29
AskatAsh <abdulaskat@gmail.com>
2023-12-06 16:13:36 +0600

    send coffee data to database using post

--- coffee-shop-server/index.js
+++ coffee-shop-server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-// connection to mongodb database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fisbs9h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,26 +25,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    app.post('/coffee', (req, res) => {
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    // const coffee = coffeeCollection.collection('coffee');
+
+    app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
-    })
 
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Coffee Shop Server is Running..!");
+  res.send("Coffee Shop Server is Running..!");
 })
 
 app.listen(port, () => {
-    console.log(`Coffee Shop server is running from port: ${port}`);
+  console.log(`Coffee Shop server is running from port: ${port}`);
 })
