b880c2ba609d277f7ebd5add7192527659db7a01
AskatAsh <abdulaskat@gmail.com>
2023-12-06 15:01:43 +0600

    used post method to send coffee details to server

--- coffee-shop-client/src/Components/AddCoffee.jsx
+++ coffee-shop-client/src/Components/AddCoffee.jsx
@@ -1,58 +1,85 @@
-
 const AddCoffee = () => {
+
+    const handleAddCoffee = event => {
+        event.preventDefault();
+        const form = event.target;
+        const coffeeName = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photoURL = form.photo.value;
+        // console.log(coffeeName, quantity, supplier, taste, category, details, photoURL);
+        const coffee = {coffeeName, quantity, supplier, taste, category, details, photoURL};
+        console.log(coffee);
+
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(coffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div className="bg-[#F4F3F0] text-[#1B1A1ACC] font-primary">
-            <form>
+            <form onSubmit={handleAddCoffee}>
 
                 <div className="flex flex-col gap-7 py-10">
                     <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
                         <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Coffee Name</label>
+                            <label htmlFor="name" className="font-semibold">Coffee Name</label>
                             <br />
                             <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                                type="text" name="name" id="coffeeName" placeholder="Add Coffee Name" />
                         </div>
                         <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Chef</label>
+                            <label htmlFor="quantity" className="font-semibold">Quantity</label>
                             <br />
                             <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
-                        </div>
-                    </div>
-                    <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
-                        <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Supplier</label>
-                            <br />
-                            <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
-                        </div>
-                        <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Taste</label>
-                            <br />
-                            <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                                type="text" name="quantity" id="quantity" placeholder="Add chef name" />
                         </div>
                     </div>
                     <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
                         <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Category</label>
+                            <label htmlFor="supplier" className="font-semibold">Supplier</label>
                             <br />
                             <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                                type="text" name="supplier" id="supplier" placeholder="Add supplier" />
                         </div>
                         <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Details</label>
+                            <label htmlFor="taste" className="font-semibold">Taste</label>
                             <br />
                             <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                                type="text" name="taste" id="taste" placeholder="Add taste" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
+                        <div className="w-full">
+                            <label htmlFor="category" className="font-semibold">Category</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="category" id="category" placeholder="Add category" />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="details" className="font-semibold">Details</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="details" id="details" placeholder="Add details" />
                         </div>
                     </div>
                     <div className="flex flex-col md:flex-row items-center justify-center px-10">
                         <div className="w-full">
-                            <label htmlFor="coffeeName" className="font-semibold">Photo</label>
+                            <label htmlFor="photo" className="font-semibold">Photo</label>
                             <br />
                             <input className="border-2 rounded-md py-2 px-2 w-full"
-                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                                type="text" name="photo" id="photo" placeholder="Add Photo URL" />
                         </div>
                     </div>
                     
