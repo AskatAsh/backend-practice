f5a5c1930b62e8a0e964227c4b8e8493d2baf9c8
AskatAsh <abdulaskat@gmail.com>
2023-12-06 13:30:22 +0600

    added responsive add coffee form with button using tailwind css

--- coffee-shop-client/src/Components/AddCoffee.jsx
+++ coffee-shop-client/src/Components/AddCoffee.jsx
@@ -1,8 +1,65 @@
 
 const AddCoffee = () => {
     return (
-        <div>
-            <h2>Add coffee</h2>
+        <div className="bg-[#F4F3F0] text-[#1B1A1ACC]">
+            <form>
+
+                <div className="flex flex-col gap-7 py-10">
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Coffee Name</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Chef</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Supplier</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Taste</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-center gap-7 px-10">
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Category</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Details</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-center px-10">
+                        <div className="w-full">
+                            <label htmlFor="coffeeName" className="font-semibold">Photo</label>
+                            <br />
+                            <input className="border-2 rounded-md py-2 px-2 w-full"
+                                type="text" name="coffeeName" id="coffeeName" placeholder="Add Coffee Name" />
+                        </div>
+                    </div>
+                    
+                    <input type="submit" value="Add Coffee" className="btn bg-[#D2B48C] border-2 border-[#331A15] mx-10" />
+                </div>
+
+            </form>
         </div>
     );
 };
