0852adfec2530df24f3db6f51f66461584543519
AskatAsh <abdulaskat@gmail.com>
2023-12-07 20:41:29 +0600

    styled coffee datails section using tailwind

--- coffee-shop-client/src/Components/Coffee.jsx
+++ coffee-shop-client/src/Components/Coffee.jsx
@@ -2,10 +2,19 @@ import { useLoaderData } from "react-router-dom";
 
 const Coffee = () => {
     const coffee = useLoaderData();
-    console.log(coffee);
+    // console.log(coffee);
+    const { coffeeName, quantity, supplier, photoURL } = coffee;
     return (
-        <div>
-            <h2>Coffee Details</h2>
+        <div className="flex flex-col md:flex-row items-center justify-center py-16 px-5 md:px-0 bg-[#F4F3F0] gap-5 md:gap-28">
+            <div className="max-w-[350px] h-[420px] flex items-center justify-center overflow-hidden">
+                <img src={photoURL} alt={`image of ${coffeeName}`} />
+            </div>
+            <div className="font-primary text-xl text-[#1B1A1A] flex flex-col gap-3">
+                <h2 className="font-secondary text-5xl text-[#331A15]">Niceties</h2>
+                <p><span className="font-semibold">Coffee Name: </span> {coffeeName}</p>
+                <p><span className="font-semibold">Supplier: </span> {supplier}</p>
+                <p><span className="font-semibold">Quantity: </span> {quantity}</p>
+            </div>
         </div>
     );
 };
