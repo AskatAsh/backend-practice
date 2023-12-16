19092f9a7ceb4ac979916692342e813afa3b3107
AskatAsh <abdulaskat@gmail.com>
2023-12-16 13:32:57 +0600

    added sweetalert to confirm delete on click

--- coffee-shop-client/src/Components/CoffeeCard.jsx
+++ coffee-shop-client/src/Components/CoffeeCard.jsx
@@ -2,11 +2,33 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FaEye } from "react-icons/fa";
 import { RiEdit2Fill, RiDeleteBin6Fill } from "react-icons/ri";
-
+import Swal from 'sweetalert2'
 
 
 const CoffeeCard = ({ coffee }) => {
     const { _id, coffeeName, quantity, supplier, photoURL } = coffee;
+
+    const handleDelete = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+    }
+
     return (
         <div className='text-[#1B1A1A] font-primary flex flex-col lg:flex-row items-center gap-8 bg-[#F5F4F1] rounded-lg p-8'>
             {/* image */}
@@ -32,7 +54,8 @@ const CoffeeCard = ({ coffee }) => {
                     </button>
                 </Link>
                 <Link>
-                    <button className="bg-[#D2B48C] w-[40px] h-[40px] flex items-center justify-center text-xl text-[#331A15]">
+                    <button onClick={() => handleDelete(_id)}
+                    className="bg-[#D2B48C] w-[40px] h-[40px] flex items-center justify-center text-xl text-[#331A15]">
                         <RiDeleteBin6Fill />
                     </button>
                 </Link>
